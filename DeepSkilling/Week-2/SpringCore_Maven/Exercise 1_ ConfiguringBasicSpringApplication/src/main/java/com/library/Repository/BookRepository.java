0e20eb12f0af9d64638a6af8357066cd1de43227
Sahastra1229 <sahastra1229@gmail.com>
2026-07-02 20:09:45 +0530

    Add files via upload

---
+++ DeepSkilling/Week-2/SpringCore_Maven/Exercise 1_ ConfiguringBasicSpringApplication/src/main/java/com/library/Repository/BookRepository.java
@@ -0,0 +1,13 @@
+package com.library.repository;
+
+
+public class BookRepository {
+
+
+    public void getBookDetails(){
+
+        System.out.println("Fetching book details from database");
+
+    }
+
+}
